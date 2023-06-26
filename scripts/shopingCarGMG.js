4e9da4736268f71d36afe69f32d9ea4344c215d5
Eliseo de Haro <elidharolpz@gmail.com>
2023-06-26 11:51:26 -0600

    Se actualizan las paginas de mis compañeros con los cambios en la pagina de shopping car y acerca de nosotros

--- scripts/shopingCarGMG.js
+++ scripts/shopingCarGMG.js
@@ -9,12 +9,9 @@ let divProduct;
 
 // productos 
 
-<<<<<<< HEAD
-=======
 // INSTANCIAS
 // ? Supongo para los usuarios???
 
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 //Selectores de Id && Clases
 //Principal Productos
 let container = document.querySelector(".product-container");//Contenedor de espacio del carrito de compras
@@ -24,15 +21,11 @@ const btnShopingCar = document.querySelector(".btn-shopingCar");//Vamos al carri
 const btnDeletProduct = document.querySelectorAll(".btn-delet");//Boton para eliminar producto
 const btnUpProduct = document.querySelectorAll(".btn-aumentar");//Boton para aumentar un producto
 const btnDownProduct = document.querySelectorAll(".btn-disminuir");//Boton para disminuir un producto
-<<<<<<< HEAD
-const btnVaciarProducts = document.querySelectorAll(".btn-void");//Boton para disminuir un producto
-=======
 const btnVaciarProducts = document.querySelectorAll(".btn-void");//Boton para vaciar carrito
 // *Estos 2 son para hacer pruebas
 const btnGetLocalStorage = document.querySelectorAll(".btn-getLocalStorage");//Boton para obtener objetos JSON
 const btnSetLocalStorage = document.querySelectorAll(".btn-setLocalStorage");//Boton para enviar objetos JSON
 const btnCleanLocalStorage = document.querySelectorAll(".btn-cleanLocalStorage");//Boton para limpiar la local storage
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 
 // carrito
 let aumento = false;
@@ -50,12 +43,9 @@ let shopingCar = [
 // }
 ];    //Vacio al inicio
 
-<<<<<<< HEAD
-=======
 let shoppingCartObject = [];
 let shoppingCartJSON = [];
 
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 console.log("Datos de inicio del carrito compras");
 console.log(shopingCar);
 
@@ -142,10 +132,7 @@ let productsArray = [   //10 objetos JavaScript de muestra
 ];
 
 //EVENTOS
-<<<<<<< HEAD
-=======
 // ? Investigar delegación de eventos
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 //Vamos al carrito de compras para ver las tarjetas de productos
 btnShopingCar.addEventListener("click", (e) => {
     e.preventDefault(); //prevenir que el navegador se actualice
@@ -160,10 +147,6 @@ btnAddProduct.forEach(function (boton) {
     boton.addEventListener("click", (e) => {
         e.preventDefault(); //prevenir que el navegador se actualice
         agregarProducto(); //llama al carrito para modificarlo
-<<<<<<< HEAD
-        enviarDatos();
-=======
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
         console.log("Se presionó boton para agregar producto");
     })
 });
@@ -175,17 +158,11 @@ btnAddProduct.forEach(function (boton) {
 btnVaciarProducts.forEach(function (boton) {
     boton.addEventListener("click", (e) => {
         e.preventDefault(); //prevenir que el navegador se actualice
-<<<<<<< HEAD
-        vaciarProductos(); //ejecuta la funcion eliminar
-=======
         vaciarProductos(); //ejecuta la funcion vaciar carrito
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
         console.log("Se presionó boton para vaciar carrito");
     })
 });
 
-<<<<<<< HEAD
-=======
 //Enviar datos
 btnSetLocalStorage.forEach(function (boton) {
     boton.addEventListener("click", (e) => {
@@ -212,7 +189,6 @@ btnCleanLocalStorage.forEach(function (boton) {
 })
 });
 
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 //FUNCIONES ESPECIALES
 // *Esta funcición ya se encuentra lista
 //Función para obtener el ID del producto segun el boton presionado
@@ -252,13 +228,6 @@ function conversionJSON(){
     return converToJSON;
 }
 
-<<<<<<< HEAD
-function enviarDatos(){
-    console.log("Esto es un objeto serializado,");
-    console.log(conversionJSON());
-    console.log("Se hizo conversión a JSON");
-}
-=======
 function converToObjetc(){
     let converToObject = (JSON.parse(getLocalStorage()));
     return converToObject;
@@ -292,7 +261,6 @@ function recibirDatos(){
     console.log("Se recibieron datos como un Objeto");
 }
 
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 /* -----------------------------------------------------------------------------------------------------*/
 //FUNCIONES NO ESPECIALES
 // *Esta funcición ya se encuentra lista
@@ -374,33 +342,17 @@ function ANTIGUAagregarProducto() {
 function crearProducto() {
     console.log("Inicia función de crear producto");
     // TODO Hacer un foreach para obtener TODOS los datos del carrito, asignarlos a las variables y pintar
-<<<<<<< HEAD
-    let productsAdd = productsArray[idProduct];
-=======
     // let productsAdd = productsArray[idProduct];
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
     //Obtengo las propiedades del objeto en variables temporales
     // let srcImg = productsAdd.src;
     // let h3Product = productsAdd.nombre;
     // let pPrice = productsAdd.precio;
-<<<<<<< HEAD
-
-    
-
-    let id = 2;
-    let cantidad = 10;
-
-    let srcImg = "https://images.pexels.com/photos/4281821/pexels-photo-4281821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-    let h3Product = "Nombre producto de prueba GMG";
-    let pPrice = 100;
-=======
     // let id = 2;
     // let cantidad = 10;
 
     // let srcImg = "https://images.pexels.com/photos/4281821/pexels-photo-4281821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
     // let h3Product = "Nombre producto de prueba GMG";
     // let pPrice = 100;
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 
     //!Verificar si la cantidad debe ir aqui
     // let inputNumber = productsAdd.cantidad;
@@ -409,8 +361,6 @@ function crearProducto() {
     const h2 = document.createElement("h2");    //creo una etiqueta h2 para el titulo del carrito de compras
     h2.classList.add('h2-product'); //Le agregamos una clase a esta etiqueta del titulo para darle estilos
     h2.textContent = "Tu carrito de compras";
-<<<<<<< HEAD
-=======
     containerShopingCar.appendChild(h2);
     let id;
     let cantidad;
@@ -419,6 +369,7 @@ function crearProducto() {
     let pPrice;
     // TODO Se cambio el objeto del carrito de compras por el objeto del local storage para pintar ahora con esta info
     // !Al aumentar datos se me suma de 5 en 5
+    // shopingCar.forEach(function(iteracionProductos){
     converToObjetc().forEach(function(iteracionProductos){
         id = iteracionProductos.id;
         srcImg = iteracionProductos.src;
@@ -426,7 +377,6 @@ function crearProducto() {
         pPrice = iteracionProductos.precio;
         cantidad = iteracionProductos.cantidad;
     
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 
     const productContainer = document.createElement("div");
     productContainer.classList.add('product-container'); //Le agregamos una clase a este div que almacenará las tarjetas
@@ -454,14 +404,9 @@ function crearProducto() {
     `;
     //Colocamos el producto en el carrito
     //Pintamos el espacio del carrito donde se van a poner los productos
-<<<<<<< HEAD
-    containerShopingCar.appendChild(h2);
-    containerShopingCar.appendChild(productContainer);
-=======
     // containerShopingCar.appendChild(h2);
     containerShopingCar.appendChild(productContainer);
 });
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 
     console.log("Cantidad de elementos ACTUAL en carrito: ", shopingCar.length);
     console.log("Finaliza función crear producto");
@@ -474,21 +419,6 @@ function modificarProducto() {
     //? Se necesita hacer una comparación de la cantidad actual de productos y se suma 1
     // Creamos una variable temporal que va almacenar el valor ACTUAL del producto(objeto) en el carrito
     // Independientemente del valor que haya (inicialmente 0), se aumentará en 1
-<<<<<<< HEAD
-    let productsAdd = shopingCar[idProduct];
-    if (aumento) {
-        productsAdd.cantidad++;
-        console.log("Se aumentó");
-        
-    } else if (decremento) {
-        if (productsAdd.cantidad > 1) {
-            productsAdd.cantidad--;
-            console.log("Se disminuyó");
-        } else {
-            productsAdd.cantidad = 1;
-        }
-    }
-=======
     // 
     let indiceProductoAEliminar = shopingCar.findIndex(function (producto) {
         //Esto nos devolverá un true o false 
@@ -513,21 +443,14 @@ function modificarProducto() {
         }
     }
 
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
     // seteamos las variables para usarse en la proxima iteración
     aumento = false;
     decremento = false;
     // Sobreescribimos la cantidad existente del carrito con esta modificación
-<<<<<<< HEAD
-    shopingCar[idProduct].cantidad = productsAdd.cantidad;
-
-    console.log("Cantidad actual del producto " + (shopingCar[idProduct].id) + " es : " + (shopingCar[idProduct].cantidad));
-=======
     // !Esta faltando que se itere sobre los elementos del carrito, o sea el find index 
     shopingCar[indiceProductoAEliminar].cantidad = productsAdd.cantidad;
 
     // console.log("Cantidad actual del producto " + (shopingCar[idProduct].id) + " es : " + (shopingCar[idProduct].cantidad));
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
     console.log(shopingCar);
     console.log("Finaliza función modificar producto");
 }
@@ -610,8 +533,6 @@ containerShopingCar.addEventListener("click", function (e) {
       console.log("Se presionó botón para aumentar cantidad de productos");
     }
   });
-<<<<<<< HEAD
-=======
   containerShopingCar.addEventListener("click", function (e) {
     if (e.target.classList.contains("btn-delet")) {
       e.preventDefault();
@@ -619,7 +540,6 @@ containerShopingCar.addEventListener("click", function (e) {
       console.log("Se presionó boton para eliminar un productos");
     }
   });
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
 // *Este evento ya se encuentra listo
 // btnUpProduct.forEach(function (boton) {
 //     boton.addEventListener("click", (e) => {
@@ -643,15 +563,6 @@ containerShopingCar.addEventListener("click", function (e) {
 
 //Eliminar producto
 // *Este evento ya se encuentra listo
-<<<<<<< HEAD
-btnDeletProduct.forEach(function (boton) {
-boton.addEventListener("click", (e) => {
-    e.preventDefault(); //prevenir que el navegador se actualice
-    eliminarProducto(); //ejecuta la funcion eliminar
-    console.log("Se presionó boton para eliminar un productos");
-})
-});
-=======
 // btnDeletProduct.forEach(function (boton) {
 // boton.addEventListener("click", (e) => {
 //     e.preventDefault(); //prevenir que el navegador se actualice
@@ -659,4 +570,3 @@ boton.addEventListener("click", (e) => {
 //     console.log("Se presionó boton para eliminar un productos");
 // })
 // });
->>>>>>> 1a9b968d06629ca8a6637fc4140622641a13c860
